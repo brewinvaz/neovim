21ed70c85bd7fe2857adf58ad853b583fdcaf8e2
Brewin Vaz <brewinvaz@users.noreply.github.com>
2024-08-10 16:05:21 -0400

    Setup lazy and colorscheme.

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
 require ("user.launch")
 require ("user.options")
 require ("user.keymaps")
+
+spec ("user.colorscheme")
+
+
+-- Initialize lazy after the plugin table is populated
+require ("user.lazy")
