e23c4f0edd7b6a8e5afe31910acf6aa7ea68d2ed
Brewin Vaz <brewinvaz@users.noreply.github.com>
2024-08-10 23:56:04 -0400

    Fix some warnings in lspconfig.lua

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ spec "user.lspconfig"
 spec "user.mason"
 spec "user.treesitter"
 spec "user.schemastore"
-spec "user.whichkey"
+-- spec "user.whichkey"
 
 -- Initialize lazy after the plugin table is defined
 require "user.lazy"
--- lua/user/lspconfig.lua
+++ lua/user/lspconfig.lua
@@ -24,7 +24,7 @@ M.on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
 
   if client.supports_method "textDocument/inlayHint" then
-    vim.lsp.inlay_hint.enable(bufnr, true)
+    vim.lsp.inlay_hint.enable(true, { bufnr })
   end
 end
 
@@ -39,7 +39,7 @@ end
 M.toggle_inlay_hints = function()
   local bufnr = vim.api.nvim_get_current_buf()
 
-  vim.lsp.inlay_hint.enable(bufnr, not vim.lsp.inlay_hint.is_enabled(bufnr))
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr }), { bufnr })
 end
 
 function M.config()
@@ -97,7 +97,7 @@ function M.config()
 
   vim.diagnostic.config(default_diagnostic_config)
 
-  for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
+  for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config() or {}, "signs", "values") or {}) do
     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
   end
 
