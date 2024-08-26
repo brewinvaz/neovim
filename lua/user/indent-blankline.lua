f851c6c7289659bfee76418563bf3e69218d7abc
Brewin Vaz <brewinvaz@users.noreply.github.com>
2024-08-26 17:43:47 -0400

    Update indent blank-line to v3

--- lua/user/indent-blankline.lua
+++ lua/user/indent-blankline.lua
@@ -1,52 +1,32 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   event = "VeryLazy",
 }
 
 function M.config()
   local icons = require "user.icons"
 
-  require("indent_blankline").setup {
-    buftype_exclude = { "terminal", "nofile" },
-    filetype_exclude = {
-      "help",
-      "startify",
-      "dashboard",
-      "lazy",
-      "neogitstatus",
-      "NvimTree",
-      "Trouble",
-      "text",
+  require("ibl").setup {
+    indent = { char = icons.ui.LineMiddle },
+    whitespace = {
+      remove_blankline_trail = true,
     },
-    -- char = icons.ui.LineLeft,
-    char = icons.ui.LineMiddle,
-    -- context_char = icons.ui.LineLeft,
-    context_char = icons.ui.LineMiddle,
-    show_trailing_blankline_indent = false,
-    show_first_indent_level = true,
-    use_treesitter = true,
-    show_current_context = true,
+    exclude = {
+      filetypes = {
+        "help",
+        "startify",
+        "dashboard",
+        "lazy",
+        "neogitstatus",
+        "NvimTree",
+        "Trouble",
+        "text",
+      },
+      buftypes = { "terminal", "nofile" },
+    },
+    scope = { enabled = false },
   }
-
-  -- indent = { char = icons.ui.LineMiddle },
-  -- whitespace = {
-  --   remove_blankline_trail = true,
-  -- },
-  --
-  -- exclude = {
-  --   filetypes = {
-  --     "help",
-  --     "startify",
-  --     "dashboard",
-  --     "lazy",
-  --     "neogitstatus",
-  --     "NvimTree",
-  --     "Trouble",
-  --     "text",
-  --   },
-  --   buftypes = { "terminal", "nofile" },
-  -- },
-  -- scope = { enabled = false },
 end
 
 return M
